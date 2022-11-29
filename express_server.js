f9d6be60e7995fb241a7bcc3888b19160bcc6f6a
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-29 01:09:26 -0800

    reorder routes so urls/:id goes after all the urls/ page

--- express_server.js
+++ express_server.js
@@ -18,17 +18,17 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render('urls_show', templateVars);
-});
-
 app.get('urls/new', (req, res) => {
   res.render('urls_new');
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render('urls_show', templateVars);
 });
 
 app.get('/hello', (req, res) => {
