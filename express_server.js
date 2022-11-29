bd47a43843579986a729189c909715f10340a1f3
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-29 01:16:43 -0800

    correct route from urls/new to /urls/new

--- express_server.js
+++ express_server.js
@@ -18,7 +18,7 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('urls/new', (req, res) => {
+app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
