9fc0b0a81dc7affa0c23c2de1bd9bfbe973cb2ba
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-29 01:07:11 -0800

    add urls/new route and lint code

--- express_server.js
+++ express_server.js
@@ -9,37 +9,41 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls', (req,res) => {
+app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:id', (req,res) => {
+app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
 });
 
-app.get('/urls.json', (req,res) => {
+app.get('urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req,res) => {
+app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get('/set', (req,res) => {
+app.get('/set', (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
 });
 
-app.get('/fetch', (req,res) => {
+app.get('/fetch', (req, res) => {
   res.send(`a = ${a}`);
 });
 
-app.listen(PORT,  () => {
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
